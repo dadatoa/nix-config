e036e3bbd9d563d24743b021e3c1bf1d4e6ef0b6
dadatoa <f0a202@outlook.com>
2025-11-06 21:20:24 +0700

    add some tools on all systems

--- modules/00-base.nix
+++ modules/00-base.nix
@@ -16,11 +16,15 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
+    just
     git
     nmap
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     skate # database key-value pair
+    starship
+    stow
+    tmux
   ];
   # allow unfree packages
   nixpkgs.config.allowUnfree = true;
