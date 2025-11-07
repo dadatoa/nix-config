736e9d1b8b07aa6ad9f1c5a8f1a827648494591e
dadatoa <f0a202@outlook.com>
2025-11-07 15:51:28 +0700

    bat

--- modules/00-base.nix
+++ modules/00-base.nix
@@ -16,6 +16,7 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
+    bat
     just
     git
     nmap
