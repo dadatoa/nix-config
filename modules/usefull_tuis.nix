99e8f8226df18724eaf99c680ceb0e4826923c12
dadatoa <f0a202@outlook.com>
2025-11-19 10:35:19 +0700

    add busybox in tools

--- modules/usefull_tuis.nix
+++ modules/usefull_tuis.nix
@@ -4,6 +4,7 @@
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     bat
+    busybox
     just
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     skate # database key-value pair
