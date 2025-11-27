9ff0b8e8611eaded06dd43031f0c30cba76e850e
dadatoa <f0a202@outlook.com>
2025-11-27 11:57:21 +0700

    netbootxyz module refactor

--- hosts/macmini/containers.nix
+++ hosts/macmini/containers.nix
@@ -1,22 +1,19 @@
 { pkgs, config, lib, ... }:
 
 {
-  virtualisation.oci-containers = {
-    backend = "docker";
-    containers = {
-      netbootxyz = {
-        volumes = [
-          "/appdata/netbootxyz/config:/config"
-          "/appdata/netbootxyz/assets:/assets"
-          ];
-        environment = {};
-        ports = [
-          "3000:3000"
-          "69:69/udp"
-          "8080:80"
+  virtualisation.oci-containers.containers = {
+    netbootxyz = {
+      volumes = [
+        "/appdata/netbootxyz/config:/config"
+        "/appdata/netbootxyz/assets:/assets"
         ];
-        image = "ghcr.io/netbootxyz/netbootxyz" ;
-        };
+      environment = {};
+      ports = [
+        "3000:3000"
+        "69:69/udp"
+        "8080:80"
+      ];
+      image = "ghcr.io/netbootxyz/netbootxyz" ;
     };
   };
 }
