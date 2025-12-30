08d75f08a6a6b85602ae85dec2ae9f64c7782b91
dadatoa <f0a202@outlook.com>
2025-12-30 20:54:37 +0700

    **macmini** config vlans pour prepa kea

--- hosts/macmini/networking.nix
+++ hosts/macmini/networking.nix
@@ -1,9 +1,6 @@
 { config, lib, pkgs, ... }:
-let 
-  vars = import ./vars.nix;
-in
 {
-  networking.hostName = vars.hostName;
+  networking.hostName = "macmini";
 
   networking.firewall.enable = false;
   networking.useDHCP = false;
@@ -16,33 +13,44 @@ in
   systemd.network.wait-online.anyInterface = true; 
 
   systemd.network = {
-    ## declare vlan
+    ## logic devices 
     netdevs = {
+      "20-vlan1" = {
+        netdevConfig = { Kind = "vlan"; Name = "vlan1"; };
+        vlanConfig = { Id = 1; };
+      };
       "20-vlan66" = {
-        netdevConfig = {
-          Kind = "vlan";
-          Name = "vlan66";
-          Description = "LAN Access";
-        };
-        vlanConfig = {
-          Id = 66;
-        };
+        netdevConfig = { Kind = "vlan"; Name = "vlan66"; };
+        vlanConfig = { Id = 66; };
+      };
+      "20-vlan100" = {
+        netdevConfig = { Kind = "vlan"; Name = "vlan100"; };
+        vlanConfig = { Id = 100; };
       };
     };
-    ## network interfaces
+    ## networks config
     networks = {
       "30-lan" = {
         enable = true;
-        matchConfig.Name = vars.interface0;
+        matchConfig.Name = "enp2s0f0";
         networkConfig.DHCP = "ipv4";
         ## add vlans on physical interface
-        vlan = [
-          "vlan66"
-        ];
+        vlan = [ "vlan66" "vlan100" "vlan1" ];
       };
       ## add virtual interfaces for vlan
-      "50-vlan66" = {
+      "30-vlan1" = { 
+        matchConfig.Name = "vlan1";
+        # address = [ "10.10.10.1/28" ];
+        networkConfig.DHCP = "ipv4";
+      };
+      "30-vlan66" = { 
         matchConfig.Name = "vlan66";
+        # address = [ "10.10.10.66/27" ];
+        networkConfig.DHCP = "ipv4";
+      };
+      "30-vlan100" = { 
+        matchConfig.Name = "vlan100";
+        # address = [ "10.10.10.100/27" ];
         networkConfig.DHCP = "ipv4";
       };
     };
